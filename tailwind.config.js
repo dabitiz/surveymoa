d9e27f6de5c8e12cea1f29acab478d5dfaaa39a6
이상민 <isangmin516@gmail.com>
2024-06-01 16:35:47 +0900

    tailwind color system 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+        colors : {
+        "primary" : "#237BF8",
+        "secondary" : "#F7CF46",
+        "success" : "#23CF69",
+        "error" : "#F82349",
+        "gray" : {
+          50 : "#F8F9FA",
+          100 : "#F4F6FA",
+          200 : "#F2F4F6",
+          300 : "#E6E9ED",
+          400 : "#D6D9DF",
+          500 : "#C5C8CD",
+          600 : "#C7C7C8",
+          700 : "#C3C4CC",
+          800 : "#9CA0A9",
+          900 : "#797E8A",
+          950 : "#101118",
+        }
+      },
+    },
   },
   plugins: [],
 }
