176d1820546fcd62f767ffc8f1285859d7b2de03
이상민 <isangmin516@gmail.com>
2024-08-01 03:01:47 +0900

    web mobile 로그인 처리 다르게 설정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,14 @@
 <script>
 	import "../app.css";
 	import { SvelteToast } from "@zerodevx/svelte-toast";
-	import { Device } from "@capacitor/device";
+
 	import { page, navigating } from "$app/stores";
 	import { invalidate } from "$app/navigation";
 	import { onMount } from "svelte";
 	import { App } from "@capacitor/app";
+	import { Capacitor } from "@capacitor/core";
 
-	import { is_mobile_app } from "@/lib/js/common.js";
 	import { update_profiles_store } from "@/lib/store/profiles_store";
-	import { operating_system, platform, update_device_store } from "@/lib/store/device_store";
 	import { loading } from "@/lib/store/global_store";
 
 	export let data;
@@ -42,7 +41,6 @@
 
 			await save_profiles_store(profiles);
 		}
-		await save_device_store();
 
 		is_initialize = true;
 		return () => {
@@ -60,13 +58,6 @@
 		await update_profiles_store("year_of_birth", profiles.year_of_birth);
 		await update_profiles_store("point", profiles.point);
 		await update_profiles_store("rating", profiles.rating);
-	};
-
-	const save_device_store = async () => {
-		const device_info = await Device.getInfo();
-
-		await update_device_store("operating_system", device_info.operatingSystem);
-		await update_device_store("platform", device_info.platform);
 	};
 
 	const handle_error = (event) => {
@@ -96,10 +87,7 @@
 
 {#if is_initialize}
 	<div class="bg-gray-200">
-		<div
-			class:md-w-half={!is_mobile_app($platform, $operating_system) && !is_url_admin($page)}
-			class="mx-auto"
-		>
+		<div class:md-w-half={Capacitor.isNativePlatform() && !is_url_admin($page)} class="mx-auto">
 			<div class="min-h-screen bg-white">
 				<slot />
 			</div>
